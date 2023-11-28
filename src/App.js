cb499ae73180ca749dbcddf913cb72c5c90af8aa
James Pogue <jamespogue@MacBook-Pro-Max.hsd1.il.comcast.net>
2023-11-28 06:29:19 -0600

    Update version.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
   const value = 'World';
-  return <div>Hello {value} - EJP</div>;
+  return <div>Hello {value} - EJP (version 2)</div>;
 }
 
 export default App;
